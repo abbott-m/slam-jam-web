42174b0fd0215a3f3639c3af2d8741c41dfdb233
abbott-m <kampadu123@gmail.com>
2025-08-26 23:48:25 +0000

    Update

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,7 @@
+// postcss.config.js
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' && {
-      cssnano: {
-        preset: ['default', {
-          discardComments: { removeAll: true },
-          normalizeWhitespace: true,
-          minifySelectors: true,
-          minifyParams: true,
-          mergeIdents: true,
-          reduceIdents: true,
-          discardUnused: true,
-          autoprefixer: false,
-        }],
-      },
-    }),
   },
 }
